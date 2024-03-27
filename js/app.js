8178340e8bbbb17c0a24257eaf1c4ebdcae8aaba
LuigiRiva99 <luigi.riva.1999@gmail.com>
2024-03-27 14:14:34 +0100

    ADD: MILESTONE 1

--- js/app.js
+++ js/app.js
@@ -3,7 +3,28 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      
+      todoList : [
+        {
+            text: 'fare la spesa',
+            done: false
+        },
+        {
+            text: 'studiare Vue',
+            done: true
+        },
+        {
+            text: 'portare fuori il cane',
+            done: false
+        },
+        {
+            text: 'andare in palestra',
+            done: false
+        },
+        {
+            text: 'leggere un libro',
+            done: true
+        },
+      ]
     }
   }
 }).mount('#app')
